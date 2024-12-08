b2297f2acedb1a701e443c8897e29685aa3889ff
nixondebantu <nixondebantu@gmail.com>
2024-12-08 16:48:16 +0600

    added schedule crud endpoint

--- server/db/tables.js
+++ server/db/tables.js
@@ -21,26 +21,38 @@ async function createTables() {
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
 
-            CREATE TABLE IF NOT EXISTS schedules(
+            CREATE TABLE IF NOT EXISTS schedules (
                 id SERIAL PRIMARY KEY,
-                user_id INT NOT NULL,
+                user_id INT NOT NULL REFERENCES users(uid),
                 start_time TIMESTAMP NOT NULL,
                 end_time TIMESTAMP NOT NULL,
                 min_duration INT NOT NULL,
                 max_duration INT NOT NULL,
-                auto_approve BOOLEAN NOT NULL DEFAULT FALSE
+                auto_approve BOOLEAN NOT NULL DEFAULT FALSE,
+                CONSTRAINT no_overlap UNIQUE (user_id, start_time, end_time)
             );
 
-            CREATE TABLE IF NOT EXISTS sessions(
-                id SERIAL PRIMARY KEY,
-                guest_id INT[] NOT NULL,
-                start_time TIMESTAMP NOT NULL,
-                end_time TIMESTAMP NOT NULL,
-                title VARCHAR(255),
-                meeting_host_url VARCHAR(255),
-                meeting_url VARCHAR(255),
-                status BOOLEAN NOT NULL DEFAULT FALSE
-            );
+
+            CREATE TABLE IF NOT EXISTS sessions (
+              id SERIAL PRIMARY KEY,
+              host_id INT NOT NULL REFERENCES users(uid),
+              guest_id INT NOT NULL REFERENCES users(uid),
+              start_time TIMESTAMP NOT NULL,
+              end_time TIMESTAMP NOT NULL,
+              title VARCHAR(255),
+              meeting_host_url VARCHAR(255),
+              meeting_url VARCHAR(255),
+              status BOOLEAN NOT NULL DEFAULT FALSE,
+              CONSTRAINT no_overlap_sessions UNIQUE (host_id, start_time, end_time)
+          );
+
+          CREATE TABLE IF NOT EXISTS schedule_availability (
+              id SERIAL PRIMARY KEY,
+              schedule_id INT NOT NULL REFERENCES schedules(id),
+              available_start TIMESTAMP NOT NULL,
+              available_end TIMESTAMP NOT NULL,
+              CONSTRAINT no_overlap_availability UNIQUE (schedule_id, available_start, available_end)
+          );
 
             `);
     console.log("Tables created successfully");
